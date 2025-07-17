94132a1dcacefe7760119ce7bef77e663381eb95
BlueBeardone <barend.army@gmail.com>
2025-07-17 13:45:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 
+// Set up EJS templating
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Main route
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
